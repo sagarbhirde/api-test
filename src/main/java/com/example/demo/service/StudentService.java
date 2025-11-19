e56e03e58b79353d60464eb6e452eb01b0645490
sagarbhirde <sagarbhirde111@gmail.com>
2025-11-20 01:50:07 +0530

    commit changes

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,8 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.StudentDTO;
-import com.example.demo.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
